b658d5b5194b883718fbbbf6f76f3dec20a9a79f
ferrandiaz <fdia13350@gmail.com>
2016-04-17 19:10:47 +0200

    async included

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express         = require("express"),
-    app             = express(),
-    bodyParser      = require("body-parser"),
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
     methodOverride = require("method-override");
 
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
 var router = express.Router();
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.send('Hello World');
 });
 app.use(router);
@@ -26,41 +26,42 @@ openstack.route('/server/:hypervisor')
 openstack.route('/meters')
     .get(compute.getHosts);
 
-app.use('/openstack',openstack);
+app.use('/openstack', openstack);
 
 require('amqplib/callback_api')
-  .connect('amqp://openstack:openstack@controller:5672//', function(err, conn) {
-    if (err != null) console.log(err);
-    consumer(conn);
-  });
+    .connect('amqp://openstack:openstack@controller:5672//', function (err, conn) {
+        if (err != null) console.log(err);
+        consumer(conn);
+    });
 
 function consumer(conn) {
-  var ok = conn.createChannel(on_open);
+    var ok = conn.createChannel(on_open);
     var ex = 'nova';
-    var q = 'notificationsQueue'
-  function on_open(err, ch) {
-    if (err != null) console.log(err);
-    ch.assertExchange(ex, 'topic', {durable: false});
-    ch.assertQueue(q);
-    ch.bindQueue(q,ex,'notifications.info')
-    ch.consume(q, function(msg) {
-      if (msg !== null) {
-        var json = JSON.parse(msg.content.toString())
-        _.each(json,function(ms){
-          var jMS = JSON.parse(ms);
-          if(jMS.event_type === 'compute.instance.create.end'){
-            console.log('Created ' + jMS.event_type);
-          }
-          if(jMS.event_type === 'compute.instance.delete.end'){
-            console.log('Deleted ' + jMS.event_type);
-          }
+    var q = 'notificationsQueue';
+
+    function on_open(err, ch) {
+        if (err != null) console.log(err);
+        ch.assertExchange(ex, 'topic', {durable: false});
+        ch.assertQueue(q);
+        ch.bindQueue(q, ex, 'notifications.info');
+        ch.consume(q, function (msg) {
+            if (msg !== null) {
+                var json = JSON.parse(msg.content.toString());
+                _.each(json, function (ms) {
+                    var jMS = JSON.parse(ms);
+                    if (jMS.event_type === 'compute.instance.create.end') {
+                        console.log('Created ' + jMS.event_type);
+                    }
+                    if (jMS.event_type === 'compute.instance.delete.end') {
+                        console.log('Deleted ' + jMS.event_type);
+                    }
+                });
+                ch.ack(msg);
+            }
         });
-        ch.ack(msg);
-      }
-    });
-  }
+    }
 }
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Server Running Port 3000');
 });
